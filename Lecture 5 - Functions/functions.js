f0a65fb1c8cd86a680d0b529ef38acfc3f75bd22
Pradyumn <pradyumnkrshukla.4672@gmail.com>
2025-07-23 22:38:33 +0530

    Lecture 5 added practice ques printSquare

--- Lecture 5 - Functions/functions.js
+++ Lecture 5 - Functions/functions.js
@@ -7,13 +7,23 @@
 //     console.log(a+b);
 // }
 
-const stringVowel = (str) => {
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
-    let count = 0 ;
-    for (let i = 0; i < str.length; i++) {
-        if (vowels.includes(str[i].toLowerCase())) {
-            count++;
-        }
-    }
-    return count;
-}
+// const stringVowel = (str) => {
+//     const vowels = ['a', 'e', 'i', 'o', 'u'];
+//     let count = 0 ;
+//     for (let i = 0; i < str.length; i++) {
+//         if (vowels.includes(str[i].toLowerCase())) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// For each function and call back functions
+let arr = [1,2,3,4,5];
+arr.forEach(function printVal(val){
+    console.log(val);
+});
+
+arr.forEach((val)=> {
+    console.log(`${val*2}`);
+});
